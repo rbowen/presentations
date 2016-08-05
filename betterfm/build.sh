6c52244302f8e0549dd891e6b7b29a10c4e8f0f0
Rich Bowen <rbowen@rcbowen.com>
2016-08-05 15:00:36 -0400

    cruft

--- betterfm/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-hovercraft betterfm.rst build
-
-# scp -r build/* people.apache.org:public_html/presentations/new_in_24
-scp -r build/* sycamore.rcbowen.com:/var/www/vhosts/www.rcbowen.com/speaks/slides/betterfm
-
